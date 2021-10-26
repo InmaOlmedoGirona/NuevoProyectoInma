c916edea465fd5671bae5982867e75419a0d2349
Inma <Inma@LAPTOP-J610EI8M>
2021-10-26 18:22:20 +0200

    Repetición del exámen para comprobar dónde me he confundido.

---
+++ ExamenRepeticion1291/src/RepitoExamen/Exam1291Repe.java
@@ -0,0 +1,11 @@
+package RepitoExamen;
+
+public class Exam1291Repe {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		System.out.println("Intento tras el exámen, por ahora mi error fue marcar Add a README file que me daba un MAIN y no un MASTER");
+
+	}
+
+}
